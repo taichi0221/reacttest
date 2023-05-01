9e902b66cae1253c666bb5b579f3e2d0655ce6a2
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-05-01 12:45:07 +0900

    fc

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  
+  return (
+  <div className='App'>
+    <h1>UseState</h1>
+    <button onClick={handleClick}>＋</button>
+    <p>{count}</p>
+  </div>
+  );
+};
+
+export default App;
